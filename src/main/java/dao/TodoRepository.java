689778018cc7a7c86735bdd98d02a20c1bb5aa9c
Son, Youngsik <ysyssom@hanmail.net>
2022-05-23 18:45:05 +0900

    dao 패키지 및 TodoRepository 생성
    dto 변수들을 dao 패키지에서 ArrayList 컬렉션에 저장하고..
    싱글턴 패턴으로 컬렉션 값들을 불러와 조건에 따른 메소드
    작성 시 활용한다.

---
+++ src/main/java/dao/TodoRepository.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.Todo;
+
+public class TodoRepository {
+	
+	// 컬렉션 ArrayList로 dto 패키지 변수들을 저장받도록 하고 객체는 todos로 명명한다.
+	private List<Todo> todos = new ArrayList<>();
+	
+	//[14~19 line] 디자인 패턴 중 생성구조 - 싱글턴 패턴을 이용하여 instance 한번 정의하고 여러번 재사용이 가능하다..
+	private static TodoRepository instance = new TodoRepository();
+	
+	public static TodoRepository getInstance() {
+		return instance;
+	}
+	
+	// dto 변수들을 컬렉션 ArrayList 저장된 값들을 불러올 때 사용한다..
+	public List<Todo> getTodos(){
+		return todos;
+	}
+}
